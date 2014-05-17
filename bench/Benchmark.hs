d591b5ce07b88d6750d3c5a58e30f7f9453572bd
Raphael Javaux <raphaeljavaux@gmail.com>
2014-05-17 19:58:06 +0200

    Remove non-SIMD benchmarks

--- bench/Benchmark.hs
+++ bench/Benchmark.hs
@@ -5,7 +5,6 @@ import Data.Int
 import Vision.Image (GreyImage)
 import qualified Vision.Image as I
 import Vision.Histogram (Histogram)
-import qualified Vision.Histogram as H
 import qualified Vision.Histogram.Simd as HS
 import Vision.Primitive
 
@@ -19,11 +18,8 @@ main = do
         !hist          = H.histogram grey Nothing :: Histogram DIM1 Int32
 
     defaultMain [
-          bgroup "histogram" [
-              bench "intersection comparison SSE 4.1" $
+          bgroup "SIMD histograms" [
+              bench "intersection comparison" $
                 whnf (HS.compareIntersectInt32 hist) hist
-            , bench "intersection comparison" $
-                whnf (H.compareIntersect hist :: Histogram DIM1 Int32 -> Int32)
-                     hist
             ]
         ]
